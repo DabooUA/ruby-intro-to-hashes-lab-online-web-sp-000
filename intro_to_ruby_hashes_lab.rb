67a20d390a34d218a0d94509223cc1e359b58926
Serhiy Bardysh <sbardysh@tutanota.com>
2020-06-01 19:36:01 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,7 +16,7 @@ def monopoly_with_second_tier
   names_hash = {}
   rent_hash = {}
 	monopoly = {
-    railroads: {pieces: 4, names: {}, rent_in_dollars: {}}
+    railroads: {pieces: 4, names: names_hash, rent_in_dollars: rent_hash}
     }
 
 end
